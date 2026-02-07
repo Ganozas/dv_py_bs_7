dd1b8ffa0a8b73153691019fe1a595ae98bacb48
Ganozas <leha.samsonov.200@gmail.com>
2026-02-07 21:28:25 +0700

    realise 0.2

--- main.py
+++ main.py
@@ -10,15 +10,16 @@ load_dotenv()
 TG_TOKEN = os.getenv('TG_TOKEN') 
 TG_CHAT_ID = os.getenv('TG_CHAT_ID')
 
-bot = ptbot.Bot(TG_CHAT_ID)
-bot.send_message(TG_TOKEN, "Привет!")
-#bot.send_message(TG_TOKEN, "Как дела?")
+
 
 
 def wait(chat_id, message):
     number = parse(message)
+    message_id = bot.send_message(TG_TOKEN, "Запускаю таймер")
     bot.create_timer(number, mirror, chat_id = chat_id, message = message)
-    counting = bot.create_countdown(number, notify_progress)
+    bot.create_countdown(number, notify_progress, chat_id = chat_id, message_id = message_id)
+    
+
 
 
 def mirror (chat_id, message):
@@ -26,12 +27,25 @@ def mirror (chat_id, message):
     print("Мне написал пользователь с ID:", chat_id)
 
 
-def notify_progress(secs_left, chat_id, message):
-    print('Осталось {} секунд'.format(secs_left))
 
 
-bot.reply_on_message(chat_id, notify_progress)
+def notify_progress(secs_left, chat_id, message_id):
+    bot.update_message(chat_id, message_id, 'Осталось {} секунд\n'.format(secs_left), render_progressbar(secs_left, 1))
+    
+
+
+def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
+    iteration = min(total, iteration)
+    percent = "{0:.1f}"
+    percent = percent.format(100 * (iteration / float(total)))
+    filled_length = int(length * iteration // total)
+    pbar = fill * filled_length + zfill * (length - filled_length)
+    return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
+
+
+
+bot = ptbot.Bot(TG_CHAT_ID)
+bot.send_message(TG_TOKEN, "Привет!")
 bot.reply_on_message(wait)
-
 bot.run_bot()
 
