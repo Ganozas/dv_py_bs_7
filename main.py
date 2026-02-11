5ffe4afd380befdf095bf38261592e014546e8a5
Ganozas <leha.samsonov.200@gmail.com>
2026-02-11 14:14:56 +0700

    realise 1.0

--- main.py
+++ main.py
@@ -12,25 +12,36 @@ TG_CHAT_ID = os.getenv('TG_CHAT_ID')
 
 
 
-
-def wait(chat_id, message):
+def reply(chat_id, message):
     number = parse(message)
-    message_id = bot.send_message(TG_TOKEN, "Запускаю таймер")
-    bot.create_timer(number, mirror, chat_id = chat_id, message = message)
-    bot.create_countdown(number, notify_progress, chat_id = chat_id, message_id = message_id)
-    
+    message_id = bot.send_message(chat_id, 'Запускаю таймер')
+    bot.create_timer(
+    number,
+    mailing,
+    chat_id = chat_id,
+    message = message
+    )
+    bot.create_countdown(
+    number,
+    notify_progress,
+    chat_id = chat_id,
+    message_id = message_id,
+    number = number
+    )   
 
 
 
-def mirror (chat_id, message):
+def mailing (chat_id, message):
     bot.send_message(chat_id, 'Время вышло!')
-    print("Мне написал пользователь с ID:", chat_id)
 
 
 
 
-def notify_progress(secs_left, chat_id, message_id):
-    bot.update_message(chat_id, message_id, 'Осталось {} секунд\n'.format(secs_left), render_progressbar(secs_left, 1))
+def notify_progress(secs_left, chat_id, message_id, number):
+    bot.update_message(
+    chat_id, 
+    message_id,
+    'Осталось {} секунд\n{}'.format(secs_left, render_progressbar(number, number - secs_left)))
     
 
 
@@ -44,8 +55,9 @@ def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='
 
 
 
-bot = ptbot.Bot(TG_CHAT_ID)
-bot.send_message(TG_TOKEN, "Привет!")
-bot.reply_on_message(wait)
-bot.run_bot()
+if __name__ == '__main__':
+    bot = ptbot.Bot(TG_CHAT_ID)
+    bot.send_message(TG_TOKEN, "Привет!")
+    bot.reply_on_message(reply)
+    bot.run_bot()
 
